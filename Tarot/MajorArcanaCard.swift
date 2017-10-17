1554c403fed597f0397f9ff975394bb0c4309eaf
Priscilla Jofani Oetomo <s3669534@w8408186.cs.rmit.edu.au>
2017-10-17 22:56:41 +1100

    Tutorial 3

--- Tarot/MajorArcanaCard.swift
+++ Tarot/MajorArcanaCard.swift
@@ -10,6 +10,45 @@ import Foundation
 
 enum MajorArcanaCard:Int
 {
+    // This initialiser is used to return a default representation if one is not asked for explicitly.
+    init()
+    {
+        self = .fool
+    }
+    
+    // This initialiser is used to request a specific card base on its rank.
+    // Note this must be declared as 'Optional' because an invalid number could be provided which would result in 'nil' being returned.
+    init?(number: Int)
+    {
+        switch number
+        {
+        case 1: self = .fool
+        case 2: self = .magician
+        case 3: self = .highPriestess
+        case 4: self = .empress
+        case 5: self = .emperor
+        case 6: self = .heirophant
+        case 7: self = .lovers
+        case 8: self = .chariot
+        case 9: self = .justice
+        case 10: self = .hermit
+        case 11: self = .wheelOfFortune
+        case 12: self = .strength
+        case 13: self = .hangedMan
+        case 14: self = .death
+        case 15: self = .temperance
+        case 16: self = .devil
+        case 17: self = .tower
+        case 18: self = .star
+        case 19: self = .moon
+        case 20: self = .sun
+        case 21: self = .judgement
+        case 22: self = .world
+        default:
+            return nil
+        }
+    }
+    
     case fool=1, magician, highPriestess, empress, emperor, heirophant, lovers, chariot, justice, hermit, wheelOfFortune, strength, hangedMan, death, temperance, devil, tower, star, moon, sun, judgement, world
     
     var rank:Int
--- Tarot/ViewController.swift
+++ Tarot/ViewController.swift
@@ -8,106 +8,190 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController
+{
     // Property referencing the label in the view
     @IBOutlet weak var lblAnswers: UILabel!
     
-    // Property referencing the image view in the storyboard
     @IBOutlet weak var imgCard: UIImageView!
-    
     // Property referencing the model for managing data and business logic
     var model = Model()
     
+    @IBOutlet weak var placeHolder: UIView!
+    
+    @IBOutlet weak var cardDescription: UILabel!
+    
+    @IBOutlet weak var faded: UILabel!
+    
+    @IBOutlet weak var btnOracle: UIButton!
+    
+    var showingBack = false
+    var front:UIImageView!
+    var back:UIImageView!
+    var cardView: UIView!
+    var direction: UIViewAnimationOptions = UIViewAnimationOptions.transitionFlipFromLeft
+    var gesturesRegistered: Bool = false
+    
+    // Property to support formatting a string
+    var attributedString:NSMutableAttributedString!
     // Respond to the user clicking a button by providing advice from the oracle
     @IBAction func askTheOracle(sender: UIButton)
     {
-        // Retrieves a random message from the oracle
-        lblAnswers?.text = model.respond()
+        lblAnswers.text = model.respond()
         
-        // Change the image in the UIImageView to the currently selected card
-        imgCard.image = UIImage(named:model.currentCard.imageName)
+        imgCard.image = UIImage(named: model.currentCard.imageName)
         
-//        var cardImageName:String = ""
-//        var message:String = ""
-//        
-//        model.respond(cardImageName: &cardImageName, message: &message)
-//        
-//        lblAnswers.text = message
-//        imgCard.image = UIImage (named:cardImageName)
+        registerGestures()
         
-//        // This variable will receive a tuple from the respond method
-//        let response :(cardImageName: String, cardMessage:String) = model.respond()
-//        
-//        lblAnswers.text = response.cardMessage
-//        
-//        imgCard.image = UIImage (named:response.cardImageName)
+        //        var cardImageName:String = ""
+        //        var message:String = ""
+        //
+        //        model.respond(cardImageName: &cardImageName, message: &message)
+        //
+        //        lblAnswers.text = message
+        //        imgCard.image = UIImage(named: cardImageName)
+        //
+        //        let response: (cardImageName:String, cardMessage:String) = model.respond()
+        //
+        //        lblAnswers.text = response.cardMessage
+        //        imgCard.image = UIImage(named: response.cardImageName)
+        
     }
     
-    override func viewDidLoad() {
+    // Lifecycle method for performing tasks after the view has loaded
+    override func viewDidLoad()
+    {
+        faded.isHidden = true
+        cardDescription.isHidden = true
+        lblAnswers.text =  "Think of a question, then click the button"
+        imgCard.image = UIImage(named: "MajorArcana.jpg")
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
+    
+    // Register gestures for swiping the card left and right
+    fileprivate func registerGestures()
+    {
+        // Only register the gestures after the user has clicked on the 'Ask Oracle' button
+        // Only register the gestures once
+        if(!gesturesRegistered)
+        {
+            gesturesRegistered = true
+            
+            /* Set up flipping behaviour.  For the flipping behaviour to work
+             * a subview must be added to the item to be flipped.  Here I used
+             * an additional image view so I can show the currently selected card
+             * on the back but it will be faded.  I hide both of these, so it doesn't
+             * matter what you set it to.*/
+            front = UIImageView(image: UIImage(named: "Fool.jpg"))
+            back = UIImageView(image: UIImage(named: "Fool.jpg"))
+            placeHolder.addSubview(back)
+            back.isHidden = true
+            front.isHidden = true
+            
+            // Add Swipe Gesture to the placeholder for the card
+            let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.handleSwipes(_:)))
+            swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+            let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(ViewController.handleSwipes(_:)))
+            swipeRight.direction = UISwipeGestureRecognizerDirection.right
+            
+            placeHolder.addGestureRecognizer(swipeLeft)
+            placeHolder.addGestureRecognizer(swipeRight)
+        }
+    }
+    
+    // Determine direction of swipe
+    func handleSwipes(_ sender:UISwipeGestureRecognizer)
+    {
+        if (sender.direction == .left)
+        {
+            direction = UIViewAnimationOptions.transitionFlipFromRight
+        }
+        if (sender.direction == .right)
+        {
+            direction = UIViewAnimationOptions.transitionFlipFromLeft
+        }
+        showCard(direction)
+    }
+    
+    // Setup card for display
+    fileprivate func showCard(_ direction: UIViewAnimationOptions)
+    {
+        if (!showingBack)
+        {
+            UIView.transition(from: front, to: back, duration: 1, options: direction, completion: nil)
+            showingBack = true
+            
+            // Change the image in the UIImageView to the currently selected card
+            imgCard.image = UIImage(named:model.currentCard.imageName)
+            cardDescription.text = model.currentCard.getCardDescription()
+            
+            lblAnswers.isHidden = true
+            faded.isHidden = false
+            cardDescription.isHidden = false
+            btnOracle.isHidden = true
+            
+            formatText()
+        }
+        else
+        {
+            UIView.transition(from: back, to: front, duration: 1, options: direction, completion: nil)
+            showingBack = false
+            
+            lblAnswers.isHidden = false
+            faded.isHidden = true
+            cardDescription.isHidden = true
+            btnOracle.isHidden = false
+        }
+    }
+    
+    func applyHighlightsToText()
+    {
+        // 1. Create a range that equals the length of the string that contains the text to be highlighted
+        let range = NSMakeRange(0, self.attributedString.length)
+        
+        // 2. Match items surrounded by single quotation marks
+        let regexStr = "(\\'\\w+(.\\s\\w+)*\\')"
+        let regex = try! NSRegularExpression(pattern: regexStr, options: [])
+        
+        // 3. Create attributes to apply to the text
+        let colourAttributes = [NSForegroundColorAttributeName : UIColor(red: 1.0, green: 0.5, blue: 0.5, alpha: 1)]
+        
+        // 4. iterate over each match, making the text red
+        regex.enumerateMatches(in: attributedString.string, options: [], range: range)
+        {
+            match, flags, stop in
+            let matchRange = match!.rangeAt(1)
+            self.attributedString.addAttributes(colourAttributes, range: matchRange)
+        }
+    }
+    
+    func applyFormattingToText()
+    {
+        let font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
+        let textColor = UIColor(red: 0.5, green: 0.5, blue: 0.5, alpha: 1)
+        
+        let attributes = [
+            NSForegroundColorAttributeName : textColor,
+            NSFontAttributeName : font,
+            ]
+        attributedString = NSMutableAttributedString(string: model.currentCard.getCardDescription(), attributes: attributes)
+    }
+    
+    fileprivate func formatText()
+    {
+        applyFormattingToText()
+        applyHighlightsToText()
+        
+        cardDescription.attributedText = attributedString
+    }
+    
+    
+    // Lifecycle method for clearing up memory resources
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
-    
-    
-    
-    
-    
-//    JSpinner number = new JSpinner(model)
-//    number.addChangeListener (new ChangeListener() {
-//        int lastValue;
-//        
-//        @Override
-//        public void stateChanged(ChangeEvent e) {
-//            if (lastValue != null && !number.getValue().equals(lastValue)) {
-//                System.out.println(number.getValue());
-//            }
-//            lastValue = number.getValue
-//        }
-//    });
-    
-    
-    
-    
-    
-    
-//    class MyPanel extends JPanel {
-//        @Override
-//        public void paintComponent(Graphics g) {
-//            super.paintComponent(g);
-//    
-//            ScalingPC scalingPC = new ScalingPC();
-//            value = scalingPC.number.getValue;
-//    
-//            Graphics2D g2 = (Graphics2D)g;
-//            ...
-//    
-//        }
-//    
-//    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 }
 
